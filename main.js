cfec95303c3188eccd7c52fbf89c4798293467c9
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-07-31 20:12:04 +0400

    test

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const apiKey = '967fc01f637f62b44ecfe691b44e394e06fd0e1377d21f3c4345046b';
+const apiKey = '10724d795dbc51';
 
 // getting user IP
 window.addEventListener('load', () => {
-    fetch('https://api.ipify.org?format=json')
+    fetch('https://ipinfo.io?token=' + apiKey)
         .then(response => response.json())
         .then(data => {
             const ip = data.ip;
@@ -12,67 +12,75 @@ window.addEventListener('load', () => {
         .catch(error => console.error('Error:', error));
 });
 
-
 function getIPDetails(ip) {
-    fetch(`https://api.ipdata.co/${ip}?api-key=${apiKey}`)
+    fetch(`https://ipinfo.io/${ip}?token=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            const latitude = data.latitude;
-            const longitude = data.longitude;
 
-            // Google Maps
-            const mapOptions = {
-                center: { lat: latitude, lng: longitude },
-                zoom: 13,
-                mapTypeControl: false,
-            };
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
-            const marker = new google.maps.Marker({         //marker custom
-                position: { lat: latitude, lng: longitude },
-                map: map,
-                icon: {
-                    url: 'img/marker.svg',
-                    scaledSize: new google.maps.Size(60, 60),
-                }
-            });
-            updateInformation(data);
+            const [latitude, longitude] = data.loc ? data.loc.split(',') : [null, null];
+            const location = `${data.city || 'Unknown City'}, ${data.country || 'Unknown Country'}`;
+            const timezone = data.timezone || 'Unknown Timezone';
+            const isp = data.org || 'Unknown ISP';
+
+            document.getElementById('location').textContent = location;
+            document.getElementById('timezone').textContent = `UTC ${timezone}`;
+            document.getElementById('isp').textContent = isp;
+
+            if (latitude && longitude) {
+                const mapOptions = {
+                    center: { lat: parseFloat(latitude), lng: parseFloat(longitude) },
+                    zoom: 13,
+                    mapTypeControl: false,
+                };
+
+                map = new google.maps.Map(document.getElementById('map'), mapOptions);
+                const marker = new google.maps.Marker({
+                    position: mapOptions.center,
+                    map: map,
+                    icon: {
+                        url: 'img/marker.svg',
+                        scaledSize: new google.maps.Size(60, 60),
+                    }
+                });
+            } else {
+                console.error('Invalid latitude, longitude');
+            }
         })
         .catch(error => console.error('Error:', error));
-
-}
-
-//information section
-function updateInformation(data) {
-    document.getElementById('location').textContent = `${data.city || 'Unknown City'}, ${data.country_name || 'Unknown Country'}`;
-    document.getElementById('timezone').textContent = data.time_zone ? `UTC ${data.time_zone.offset.slice(0, 3)}:${data.time_zone.offset.slice(3)}` : 'Unknown Timezone';
-    document.getElementById('isp').textContent = data.asn ? data.asn.name : 'Unknown ISP';
 }
 
 // Initialize google map
 function initMap() {
     const defaultLatitude = 37.7749;
     const defaultLongitude = -122.4194;
+    const mapOptions = {
+        center: { lat: defaultLatitude, lng: defaultLongitude },
+        zoom: 13,
+        mapTypeControl: false,
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
+
 //ip input
 const ipForm = document.querySelector('form');
 ipForm.addEventListener('submit', function (event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const ip = formData.get('ip-input');
-    
+
     //validation IP 
     const ipFormatRegex = /^(\d{1,3}\.){3}\d{1,3}$/;
     if (!ipFormatRegex.test(ip)) {
-        customAlert();
+        showAlertMessage();
         return;
     }
     getIPDetails(ip);
 });
 
 //alert message
-function customAlert() {
-    const alertMessage= document.getElementById('alert');
+function showAlertMessage() {
+    const alertMessage = document.getElementById('alert');
     alertMessage.style.display = 'flex';
 
     const closeButton = document.getElementById('close-alert');
